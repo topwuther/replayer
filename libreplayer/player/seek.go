32098fb559fb11ef6b66a5d8457f1eb98c189d14
topwuther <topwuther@gmail.com>
2024-06-02 12:59:19 +0000

    optimization music seek function.

--- libreplayer/player/seek.go
+++ libreplayer/player/seek.go
@@ -1,24 +1,19 @@
 package player
 
-import (
-	"os"
-
-	"github.com/hajimehoshi/go-mp3"
-)
-
 func (p *Player) Seek(offset float32, whence int) int64 {
 	var now int64
-	p.Stop()
-	p.file, p.Err = os.Open(musicdir + p.music.Filename)
+	if p.decoder == nil {
+		return 0
+	}
+	length := p.decoder.Length()
+	current := int64(float32(length) * offset)
+	current = current - (current % 4)
+	now, p.Err = p.decoder.Seek(current, whence)
 	if p.Err != nil {
 		return 0
 	}
-	p.decoder, p.Err = mp3.NewDecoder(p.file)
-	now, p.Err = p.decoder.Seek(int64(float32(p.decoder.Length())*offset), whence)
-	if p.Err != nil {
-		return 0
+	if !p.IsPlaying() {
+		p.Play(p.music)
 	}
-	p.Pause()
-	p.Resume()
 	return now
 }
